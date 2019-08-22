97a5d9177febb628c417ec77b09db4a2490b3b41
gin00001 <53989631+gin00001@users.noreply.github.com>
2019-08-23 05:23:21 +0800

    Update demo2.vue

--- components/stepper/demo/cases/demo2.vue
+++ components/stepper/demo/cases/demo2.vue
@@ -2,13 +2,14 @@
   <div class="md-example-child md-example-child-stepper md-example-child-stepper-2">
     <md-field>
       <md-field-item title="最小值-12，最大值18">
-        <md-stepper slot="right" v-model="value" min="-12" max="18" />
+        <md-stepper slot="right" v-model="value" min="10" max="18" />
       </md-field-item>
     </md-field>
   </div>
 </template>
 
-<script>import {Stepper, Field, FieldItem} from 'mand-mobile'
+<script>
+import {Stepper, Field, FieldItem} from 'mand-mobile'
 
 export default {
   name: 'stepper-demo',
@@ -27,4 +28,5 @@ export default {
     }
   },
 }
-</script>
+
+</script>
